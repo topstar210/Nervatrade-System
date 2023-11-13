5c83335489293942267454c512ce1afa3fd67c68
topstar210 <happycamper0210@gmail.com>
2023-11-13 22:38:22 +0300

    chore: fix logo issue on server

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         <NextAuthProvider>
           <main className="min-h-screen lg:flex">
             <div className="lg:w-5/12 flex items-center justify-center py-10 bg-dark-second bg-authbg-pattern bg-no-repeat bg-left-bottom relative min-h-screen">
-              <img src="/logo.png" className="absolute top-10 left-12 md:top-16 md:left-20" alt="Logo" />
+              <img src="/images/logo.png" className="absolute top-10 left-12 md:top-16 md:left-20" alt="Logo" />
               <img src="/images/auth-star.png" className="absolute top-20 right-10" alt="auth-starpattern" />
               <div className="px-10 max-w-[490px]">
                 <p>
