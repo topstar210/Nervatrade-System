e14ebe2f838fc65b18bd026f8d218b8405cd7c67
topstar210 <happycamper0210@gmail.com>
2023-11-13 10:26:36 +0300

    update db path type

--- app/utilities/dbConnect.ts
+++ app/utilities/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function dbConnect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI! + process.env.DB_NAME);
+    await mongoose.connect(process.env.MONGODB_URI!);
   } catch (error) {
     throw new Error("Connection failed!");
   }
